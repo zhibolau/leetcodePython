e734050e420e661b6b461df42b2abe1fd3b600c1
Zhibo Liu <zhiboli@amazon.com>
2017-09-04 14:26:35 -0700

    1.twoSum

--- 1.twoSum.py
+++ 1.twoSum.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        Given nums = [2, 7, 11, 15], target = 9,
+        Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
         """
         dict = {}
         list = []
