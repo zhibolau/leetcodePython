e67005184d494142b590f301217986d048831a5c
zhibolau <zhibolau@gmail.com>
2023-06-30 16:57:52 -0700

    Update 543. Diameter of Binary Tree.py

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#1.最长条路径经过根节点，那么只需要找出根节点的左右两棵子树的最大深度然后相加即可。 
+#2.最长路径没有经过根节点，那么只需要找出根节点的左子树或者根节点的右子树作为根的
+#最长路径度即可。
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.max = 0
