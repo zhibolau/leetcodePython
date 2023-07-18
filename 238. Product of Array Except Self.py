bc4f399f99099d81fd420a1456940e643a9faa49
zhibolau <zhibolau@gmail.com>
2023-07-18 13:24:24 -0700

    Update 238. Product of Array Except Self.py

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -14,3 +14,19 @@ class Solution:
                     product *= nums[j]
             res.append(product)
         return res
+
+# O N 1
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        size = len(nums)
+        res = [1] *size
+        pre_p = post_p = 1
+        # 将第 i 个位置乘上前 i - 1 个数的积
+        for i in range(size):
+            res[i] *= pre_p
+            pre_p *= nums[i]
+# 将第 i 个位置乘上后面数的积
+        for i in range(size-1,-1,-1):
+            res[i] *= post_p
+            post_p *= nums[i]
+        return res
