c487b9dcd3893dfdb8ef5977bc6591ff11eb1a17
zhibolau <zhibolau@gmail.com>
2023-07-18 21:57:37 -0700

    Create 39. Combination Sum.py

---
+++ 39. Combination Sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates = sorted(candidates)
+        res = []
+        self.dfs(candidates,target,0,[],res)
+        return res
+
+    def dfs(self,candidates,target,start,combinations,res):
+        if target == 0:
+            res.append(list(combinations))
+            return res
+        for i in range(start,len(candidates)):
+            if candidates[i] > target:
+                return
+            combinations.append(candidates[i])
+            self.dfs(candidates,target-candidates[i],i,combinations,res)
+            combinations.pop()
