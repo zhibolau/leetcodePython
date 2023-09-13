ed0908aa489ba2194b225c5581a5c6a862aef175
zhibolau <zhibolau@gmail.com>
2023-09-12 21:03:29 -0700

    Update 50. Pow(x, n).py

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -8,9 +8,9 @@ class Solution:
             return self.dfs(x, n)
     
     def dfs(self,x, n):
-        if n == 1:
+        if n == 1: #base case
             return x
-        res = self.dfs(x, n //2) 
+        res = self.dfs(x, n //2)  # so below could use res * res based on n odd or even
         if n %2 == 0:
             return res * res
         else:
