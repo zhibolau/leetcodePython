6c62ad8e11b4b4721702cb8dbaa2e95a310df98f
zhibolau <zhibolau@gmail.com>
2023-09-20 22:12:13 -0700

    Create 200. Number of Islands.py

---
+++ 200. Number of Islands.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        res = 0
+        for x in range(rows):
+            for y in range(cols):
+                if grid[x][y] == '1':
+                    self.bfs(grid,x,y) #找到周边都是1的区域
+                    res += 1
+        return res
+
+    def bfs(self,grid,x,y):
+        queue = deque([(x,y)])
+        grid[x][y] == '0' # 设置成0 说明已经计算过他了，本来他是1
+        while queue:
+            x,y = queue.popleft()
+            for delta_x,delta_y in [(1,0),(0,-1),(0,1),(-1,0)]:
+                new_x = x + delta_x
+                new_y = y+delta_y
+                if not self.valid(grid,new_x,new_y):
+                    continue #发现0说明到边界了
+                queue.append((new_x,new_y))
+                grid[new_x][new_y] = '0' # 设置成0 说明已经计算过他了，本来他是1
+
+
+    def valid(self,grid,x,y):
+        rows = len(grid)
+        cols = len(grid[0])
+        return x >= 0 and x < rows and y >= 0 and y < cols and grid[x][y] == '1'
