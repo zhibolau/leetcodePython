05da78937e269dcfd8ae5e13455ff36e9de41137
zhibolau <zhibolau@gmail.com>
2023-10-04 09:41:04 -0700

    Update 238. Product of Array Except Self.py

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -30,3 +30,14 @@ class Solution:
             res[i] *= post_p
             post_p *= nums[i]
         return res
+
+"""
+观察可以发现，每个数其实就是它前面部分的积乘以后面部分的积。所有前缀的积，通过一次遍历即可算出来，后缀也是一样。
+我们可以边乘边算前缀积，同时将他乘在结果数组上，这样达到了O(1)的额外空间，同时不用到除法。
+
+令result为一个全是1的数组。
+令前缀积prefixProduct等于1，后缀积postfixProduct等于1。
+正序遍历数组nums，第i个位置先将结果乘上prefixProduct，再将prefixProduct乘上nums[i]。
+逆序遍历数组nums，第i个位置先将结果乘上postfixProduct，再将postfixProduct乘上nums[i]。
+返回result。
+"""
