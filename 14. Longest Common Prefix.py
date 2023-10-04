be1895228c26a165d3982a502946ee2a69763616
zhibolau <zhibolau@gmail.com>
2023-10-04 10:20:58 -0700

    Create 14. Longest Common Prefix.py

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        if not str:
+            return ""
+        
+        for i in range(len(strs[0])):
+            for string in strs[1:]:
+                if i >= len(string) or string[i] != strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
