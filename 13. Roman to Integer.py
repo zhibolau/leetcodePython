3bbc3841c2b8ad5a4e34a47800d3ff723fdd83b5
zhibolau <zhibolau@gmail.com>
2023-10-04 09:59:54 -0700

    Create 13. Roman to Integer.py

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s: return 0
+        d = {'I':1,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+        for i in range(len(s)-1):
+            if d[s[i]]<d[s[i+1]]:
+                res -=d[s[i]]
+            else:
+                res +=d[s[i]] #这里包括大于等于的情况 比如 III，所以顺序不能错
+        return  res+d[s[-1]]
+    
+
+"""
+模拟罗马数字运算 举个例子就懂
+IV
+"""
