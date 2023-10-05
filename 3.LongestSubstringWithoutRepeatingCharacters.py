a2ad32184c67d29a5590d393f60913d949c9fd44
zhibolau <zhibolau@gmail.com>
2023-10-05 14:38:12 -0700

    Update 3.LongestSubstringWithoutRepeatingCharacters.py

--- 3.LongestSubstringWithoutRepeatingCharacters.py
+++ 3.LongestSubstringWithoutRepeatingCharacters.py
@@ -9,7 +9,7 @@ class Solution(object):
         d = {}
         for i,ch in enumerate(s):
             if ch in d and d[ch]>=left:
-                left = d[ch]+1 #出现重复了  挪动左pointer
+                left = d[ch]+1 #出现重复了  需要比较重复之后的会不会出现比之前还长的，所以拿到当前的index：也就是下一位 挪动左pointer
             d[ch]=i #记录当前值的index
             res = max(res,i-left+1)
         return res    
