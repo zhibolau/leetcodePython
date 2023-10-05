094c710332ddf1845b5d1c480ce0281872f75a3f
zhibolau <zhibolau@gmail.com>
2023-10-05 15:09:55 -0700

    Update 57. Insert Interval.py

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -1,10 +1,10 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        for index,interval in enumerate(intervals):
-            if interval[1] < newInterval[0]:
+        for index,interval in enumerate(intervals): 
+            if interval[1] < newInterval[0]:#
                 res.append(interval)
-            elif interval[0] > newInterval[1]:
+            elif interval[0] > newInterval[1]: #此时把新的放入res最前面即可 因为新的比最小的还小，后面不用查看
                 res.append(newInterval)
                 return res+intervals[index:]
             else:#有交集
