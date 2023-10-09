db280a5f7affc7c93821d93bdf052ec40386353a
zhibolau <zhibolau@gmail.com>
2023-10-09 10:10:17 -0700

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -15,3 +15,28 @@ class Solution(object):
                 res.append([num]+r)
                 
         return res
+    
+#找到所有数组元素的组合
+
+
+#dfs法
+class Solution:
+    """
+    @param: nums: A list of integers.
+    @return: A list of permutations.
+    """
+    def permute(self, nums):
+        output = []
+        self.dfs(nums, [], output)
+        return output
+    
+    def dfs(self, nums, workingSet, output):
+        if len(nums) == len(workingSet):
+            output.append(list(workingSet))
+            return
+        
+        for num in nums:
+            if num not in workingSet:
+                workingSet.append(num)
+                self.dfs(nums, workingSet, output)
+                workingSet.pop()
