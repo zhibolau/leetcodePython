e7d5582772b1f8342fea788adce377023aaaf07a
zhibolau <zhibolau@gmail.com>
2023-10-09 19:11:16 -0700

    Update 33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -9,11 +9,11 @@ class Solution:
             elif nums[mid] >= nums[left]: #若mid左边是排好序的
                 if  nums[left]<= target < nums[mid]:
                     right = mid -1
-                else:
+                else: #target >nums[mid]
                     left = mid + 1
             else:#若mid右边是排好序的
                 if nums[mid]  < target <= nums[right]:
                     left = mid + 1
-                else:
+                else: #target <nums[mid]
                     right = mid - 1
         return -1
