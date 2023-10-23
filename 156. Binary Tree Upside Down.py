c5c384579e1c93ac596ca5aeb7583efd7f5d1dbb
zhibolau <zhibolau@gmail.com>
2023-10-23 16:34:35 -0700

    Create 156. Binary Tree Upside Down.py

---
+++ 156. Binary Tree Upside Down.py
@@ -0,0 +1,62 @@
+
+    
+#recur
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the root of binary tree
+    @return: new root
+    """
+    def upsideDownBinaryTree(self, root):
+        if root is None:
+            return None
+        return self.dfs(root)
+        
+    def dfs(self, root):
+        if root.left is None:
+            return root
+        newRoot = self.dfs(root.left)
+        root.left.right = root
+        root.left.left = root.right
+        root.left = None
+        root.right = None
+        
+        return newRoot
+    
+
+#iteration
+class Solution:
+    """
+    @param root: the root of binary tree
+    @return: new root
+    """
+    def upsideDownBinaryTree(self, root):
+        # write your code here
+
+        # this idea is similar to flip linked list
+        if not root:
+          return None
+
+        prev = None
+        prev_right = None
+        head = root
+        while head:
+          # store temp
+          temp_left = head.left
+          temp_right = head.right
+          # flip
+          head.left=prev_right
+          head.right = prev
+          # move next
+          prev = head #之前走过的点
+          prev_right = temp_right #之前的右为 暂时的右
+          head = temp_left # head根据题意为temp——left
+
+        return prev
