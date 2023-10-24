d9db368ff04010a77c9583d21c7f6dcb60c0b3c3
zhibolau <zhibolau@gmail.com>
2023-10-24 11:53:14 -0700

    Update 739. Daily Temperatures.py

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -14,19 +14,3 @@ class Solution:
         return res
 
         
-#method 2
-    def champagneTower(self, poured, query_row, query_glass):
-        # write your code here
-
-        # f[i][j]: arrived champagne at (i,j)
-        f = [[0 for j in range(query_row + 1)] for i in range(query_row + 1)]
-        f[0][0] = poured
-
-        for i in range(1, query_row + 1):
-            f[i][0] = 0.5 * max(f[i - 1][0] - 1, 0)
-            for j in range(1, min(i + 1, query_row + 1)):
-                f1 = max(f[i - 1][j] - 1, 0)
-                f2 = max(f[i - 1][j - 1] - 1, 0)
-                f[i][j] = (f1 + f2) * 0.5
-
-        return float(min(f[query_row][query_glass], 1))
