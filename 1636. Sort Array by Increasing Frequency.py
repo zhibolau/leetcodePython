7168cddf9ca54310959c750b128a9e4645708943
zhibolau <zhibolau@gmail.com>
2023-10-26 10:12:11 -0700

    Update 1636. Sort Array by Increasing Frequency.py

--- 1636. Sort Array by Increasing Frequency.py
+++ 1636. Sort Array by Increasing Frequency.py
@@ -11,7 +11,7 @@ class Solution:
 
             # 将列表中的元素作为key,出现次数作为value,存到字典中
             my_dict[item] = c
-        # 将字典按照value的大小升序排序,如果有多个值相同,按照key本身降序排序
+        # 将字典按照value的大小升序排序--- x[1],如果有多个值相同,按照key本身降序排序 --- x[0] * (-1)
         my_dict = sorted(my_dict.items(), key=lambda x: (x[1], x[0] * (-1)))
         for i in my_dict:
             for j in range(i[1]):
