4a7437e161e62061c1387a6b9e640966d5a0e781
zhibolau <zhibolau@gmail.com>
2023-10-30 12:46:47 -0700

    Update 179. Largest Number.py

--- 179. Largest Number.py
+++ 179. Largest Number.py
@@ -25,3 +25,4 @@ class Solution(object):
 
 #也可以写成这样
 #nums.sort(key=cmp_to_key(lambda x,y: 1 if str(x)+str(y) >  str(y)+str(x) else -1),reverse=True)
+#https://blog.csdn.net/weixin_46447549/article/details/128159867?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0-128159867-blog-123118854.235^v38^pc_relevant_anti_vip_base&spm=1001.2101.3001.4242.1&utm_relevant_index=3
