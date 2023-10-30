09be6b1c70783c31f176e8ab57291a183c67220f
zhibolau <zhibolau@gmail.com>
2023-10-30 12:51:41 -0700

    Update 151. Reverse Words in a String.py

--- 151. Reverse Words in a String.py
+++ 151. Reverse Words in a String.py
@@ -16,3 +16,12 @@ class Solution(object):
         for i in range(len(stack)):
             res += stack.pop()+" "
         return res[:-1]
+    
+
+#method 2
+class Solution:
+    # @param s : A string
+    # @return : A string
+    def reverseWords(self, s):
+        #strip()去掉s头尾的空格，split()按照空格分割字符串，reversed翻转，''.join按照空格连接字符串
+        return ' '.join(reversed(s.strip().split()))
