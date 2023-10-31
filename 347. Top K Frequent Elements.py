ff72e3dfa841a1e708c07c2f6f0aa006711fa8af
zhibolau <zhibolau@gmail.com>
2023-10-31 15:19:12 -0700

    Update 347. Top K Frequent Elements.py

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,14 +1,18 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_dict = {}
-        for num in nums:
-            freq_dict[num] = freq_dict.get(num, 0) + 1
-            
-        # 按照频率进行排序
-        freq_dict_sorted = sorted(freq_dict.items(), key=lambda x: x[1], reverse=True)
-        
-        # 取前k个元素返回
-        ret = list()
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in nums:
+            d[i]=d.get(i,0) +1
+
+        sort_nums=sorted(d.items(), key=lambda x: x[1],reverse=True)
+
+        res = []
         for i in range(k):
-            ret.append(freq_dict_sorted[i][0])
-        return ret
+            res.append(sort_nums[i][0])
+
+        return res
