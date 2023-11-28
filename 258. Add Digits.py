e096486dd1fd38e2376c582738c9c1a9147279e6
zhibolau <zhibolau@gmail.com>
2023-11-28 11:55:33 -0800

    Update 258. Add Digits.py

--- 258. Add Digits.py
+++ 258. Add Digits.py
@@ -5,3 +5,17 @@ class Solution:
 
         return (num - 1) % 9 + 1
         
+
+
+class Solution:
+    """
+    @param num: a non-negative integer
+    @return: one digit
+    """
+    def addDigits(self, num):
+        # write your code here
+        if num <= 9:
+            return num
+        else:
+            num = sum(int(n) for n in list(str(num)))
+            return self.addDigits(num)
