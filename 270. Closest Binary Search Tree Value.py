286e2ea75e57965b31aebb2f9741aaee3e10f9ef
zhibolau <zhibolau@gmail.com>
2023-11-30 09:34:53 -0800

    Create 270. Closest Binary Search Tree Value.py

---
+++ 270. Closest Binary Search Tree Value.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def closestValue(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
+        upper = root
+        lower = root
+        while root:
+            if target > root.val:
+                lower = root
+                root = root.right
+            elif target < root.val:
+                upper = root
+                root = root.left
+            else:
+                return root.val
+        if abs(upper.val - target) < abs(lower.val - target):
+            return upper.val
+        return lower.val
